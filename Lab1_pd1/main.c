67bce2bba8ceb92237f222a135314c78ac1f8ca1
MarLeo <martintchokonthe@yahoo.fr>
2016-04-30 20:25:05 +0200

    Initial Commit

---
+++ Lab1_pd1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab1_pd1
+//
+//  Created by Martin Tchokonthe on 30/04/16.
+//  Copyright © 2016 Martin Tchokonthe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
